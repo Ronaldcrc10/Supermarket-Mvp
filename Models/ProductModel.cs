44702ef27198e115e2dd0ceba8dc9ccdfae2a43c
Ronaldo <ronaldcollo172@gmail.com>
2024-10-14 15:52:19 -0500

    Product View

---
+++ Models/ProductModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Supermarket_mvp.Models
+{
+    internal class ProductModel
+    {
+        [DisplayName("Product Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Product name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Product Price")]
+        [Required(ErrorMessage = "Product price is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Product price must be greater than zero")]
+        public int Price { get; set; }
+
+        [DisplayName("Product Stock")]
+        [Required(ErrorMessage = "Product stock is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Product stock must be zero or greater")]
+        public int Stock { get; set; }
+    }
+}
--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217c93ed973dbf307fbed9d430f3051c601c5e79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b139e4698d92c590a364417e457d6c55e03a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
