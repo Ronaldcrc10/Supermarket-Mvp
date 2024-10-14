87b139e4698d92c590a364417e457d6c55e03a47
Ronaldo <ronaldcollo172@gmail.com>
2024-10-14 15:05:10 -0500

    Creating Table Product

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿use Supermarket
+create table Products
+(
+ Product_Id int identity (100000,1) primary key,
+ Product_Name nvarchar (50) not null,
+ Product_Price int not null,   
+ Product_Stock int NOT NULL
+)
+go
+
+INSERT INTO Products values ('Apple', 100, 50)
+INSERT INTO Products values ('Banana', 80, 100)
--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e2808c82dffa7990f11f094db99ab04d88ea7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217c93ed973dbf307fbed9d430f3051c601c5e79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
